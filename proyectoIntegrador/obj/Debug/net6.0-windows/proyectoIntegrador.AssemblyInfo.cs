a5a60ac5922e778866dc5423b74038b18de92b3f
illeiva2 <illeiva@outlook.com>
2024-10-12 17:56:47 -0300

    fix

--- proyectoIntegrador/obj/Debug/net6.0-windows/proyectoIntegrador.AssemblyInfo.cs
+++ proyectoIntegrador/obj/Debug/net6.0-windows/proyectoIntegrador.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoIntegrador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a4fd9b3ddc0c791d7ac063ac6f4bc2a99301df3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a77b55a8902f6ce64682dad6260dfbce04615d63")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoIntegrador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoIntegrador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
