3aa6aa0664802a5a98ac97783d7dc5d600143819
Ignacio Toledo Roll <rolltoledoignacio@gmail.com>
2024-10-12 18:24:22 -0300

    Agrego los cambios de ivan

--- proyectoIntegrador/obj/Debug/net6.0-windows/proyectoIntegrador.AssemblyInfo.cs
+++ proyectoIntegrador/obj/Debug/net6.0-windows/proyectoIntegrador.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoIntegrador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a77b55a8902f6ce64682dad6260dfbce04615d63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7814709faf11ecd576dc325e26eb48fa85231ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoIntegrador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoIntegrador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
